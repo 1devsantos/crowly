7d754ca7b68ca72ece40949986782d481a9f772d
Filipe Santos <filipe.devsantos@hotmail.com>
2024-08-03 15:18:53 -0300

    set routes

--- lib/app/app_widget.dart
+++ lib/app/app_widget.dart
@@ -1,4 +1,9 @@
+import 'package:crowly/core/routes/app_routes.dart';
 import 'package:crowly/core/theme/app_theme.dart';
+import 'package:crowly/views/home_view.dart';
+import 'package:crowly/views/sign_in_view.dart';
+import 'package:crowly/views/sign_up_view.dart';
+import 'package:crowly/views/welcome_view.dart';
 import 'package:flutter/material.dart';
 
 class AppWidget extends StatelessWidget {
@@ -10,6 +15,12 @@ class AppWidget extends StatelessWidget {
       darkTheme: AppTheme.darkTheme,
       theme: AppTheme.lightTheme,
       themeMode: ThemeMode.dark,
+      routes: {
+        AppRoutes.welcome: (context) => const WelcomeView(),
+        AppRoutes.signIn: (context) => const SignInView(),
+        AppRoutes.signUp: (context) => const SignUpView(),
+        AppRoutes.home: (context) => const HomeView(),
+      },
     );
   }
 }
